c4888627a2a120199a95bd123f063d499f7d7457
Chen Yuequn <xinxiao0768@gmail.com>
2012-05-30 18:03:53 +0800

    mod:Interation.*

--- src/Interaction.cpp
+++ src/Interaction.cpp
@@ -51,7 +51,7 @@ void Interaction::setXY(int coordx, int coordy) {
 }
 
 
-void Interaction::calD_l() {
+void Interaction::calPencilB_v() {
 
     set<Elem>::iterator iter = pencil.getAllPoints.begin();
 
@@ -62,26 +62,59 @@ void Interaction::calD_l() {
         float hmax = paper.getH_max(tempx, tempy);
         float hmin = paper.getH_min(tempx, tempy);
         
+        // 计算d_l
         float d_l = hmax - hmax * pencil.getAvgPressure();
         if (d_l < hmin)
             d_l = hmin;
-        
+
+        // 更新突起高度
+        float E = d_l * pencil.da() * paper.getW();
+        float h = paper.getH(tempx, tempy) - E;
+        paper.update(h, tempx, tempy);
+         
+        // 计算突起对应铅笔位置上的损耗 
         if (paper.getH(tempx, tempy) >= d_l)
-            iter->B_v = paper.getT_v(tempx, tempy);
+            iter->b = paper.getT_v(tempx, tempy);
         else {
 
             float h_m = (paper.getHp_max(tempx, tempy) - d_l) /
                         (paper.getHp_max(tempx, tempy) - paper.getHp_min(tempx, tempy));
-            iter->B_v = paper.getT_v(tempx, tempy) * h_m;
+            iter->b = paper.getT_v(tempx, tempy) * h_m;
         }
+        
+        // 调整B_v
+        pencil.BVAdjuster(iter->b);
 
-        pencil.BVAdjuster(B_v);
-        ////////////////////////////////////////////
-        float B_k = B_v * paper.getTop(tempx, tempy);       
-        float g_k, G_k = 0;
+    }
 
-        for (int i = 0; i < 4; ++i) {
-            g_k = pencil.getGP() * B_k;
+
+    iter = pencil.getAllPoints.begin();
+    // 计算4个位置
+    float B = 0;
+    for (; iter != pencil.getAllPoints.end(); ++iter) {
+        int tempx = x + iter->x;   
+        int tempy = y + iter->y;
+
+        Grain* G = relativeGrains(tempx, tempy);
+        float currentH = paper.getH(tempx, tempy);
+        for (int i = 0; i < 4; ++i, ++G)
+            B += G->getB_v() * G->calD_k(currentH);
+
+        if (paper.getT_v(tempx, tempy) >= paper.getL_k(tempx, tempy))   
+            B = B * pow(paper.getM(), pencil.getAvgPressure());
+
+        float G = pencil.getGP() * B;
+        float C = pencil.getCP() * B;
+        float W = pencil.getWP() * B;
+
+        float T = G + C + W;
+        paper.updateT_k(tempx, tempy, T);
+
+    }
+
+    
+
+             
 
 
 
